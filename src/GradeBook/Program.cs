0fbdcb7460d75a28f2f8015fcdadf461c8a13b96
adarshsingh1809 <adarshkr.singh789@gmail.com>
2021-05-30 22:45:25 +0530

    Commit

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,19 +7,15 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var book = new Book();
+            var book = new Book("Adarsh's GradeBook");
+            book.AddGrade(33.1);
+            book.AddGrade(90.2);
+            book.AddGrade(77.5);
+            var stats = book.GetStatistics(); 
 
-            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1};
-            grades.Add(56.1);
-            
-
-            var result = 0.0;
-            foreach(var number in grades)
-            {
-                result+=number;
-            }
-            result /=grades.Count;
-            Console.WriteLine($"The average grade is {result:N1}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The higest grade is {stats.High}");
+            Console.WriteLine($"The Average grade is {stats.Average:N1}");
         }
     }
 }
