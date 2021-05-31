94b48a058468a85d044fefca955aacc664527e58
adarshsingh1809 <adarshkr.singh789@gmail.com>
2021-05-31 16:25:57 +0530

    commit

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -5,6 +5,85 @@ namespace GradeBook.Tests
 {
     public class TypeTests
     {
+      [Fact]
+        public void ValueTypesAlsoPassByValue()
+        {
+          var x = GetInt();
+          SetInt(ref x);
+
+
+          Assert.Equal(42, x);
+        }
+
+        private void SetInt(ref int z)
+        {
+            z = 42;
+        }
+
+        private int GetInt()
+        {
+          return 3;
+        }
+
+      [Fact]
+        public void CSharpCanPassByRef()
+        {
+          var book1 = GetBook("Book 1");  
+          GetBookSetName(out book1, "New Name"); 
+
+          Assert.Equal("New Name", book1.Name);  
+        }
+
+        private void GetBookSetName(out Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+       [Fact]
+        public void CSharpCanPassByValue()
+        {
+          var book1 = GetBook("Book 1");  
+          GetBookSetName(book1, "New Name"); 
+
+          Assert.Equal("Book 1", book1.Name);  
+        }
+
+        private void GetBookSetName(Book book, string name)
+        {
+          book = new Book(name);
+          book.Name = name;
+        }
+
+        [Fact]
+        public void CanSetNameByReference()
+        {
+          var book1 = GetBook("Book 1");  
+          SetName(book1, "New Name"); 
+
+          Assert.Equal("New Name", book1.Name);  
+        }
+
+        private void SetName(Book book, string name)
+        {
+          book.Name = name;
+        }
+        
+        [Fact]
+        public void StringBehavesLikeValueTypes()
+        {
+          string name = "Adarsh";
+          var upper = MakeUpperCase(name);
+
+
+          Assert.Equal("Adarsh", name);
+          Assert.Equal("ADARSH", upper);
+        }
+        private string MakeUpperCase(string parameter)
+        {
+          return parameter.ToUpper();
+        }
+
+
         [Fact]
         public void GetBookReturnsDifferentObjects()
         {
